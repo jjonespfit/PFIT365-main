a663b03c712953cec4cfa60e4f2302b043e5dda2
jjonespfit <jordan.jones@pathforwardit.com>
2023-07-12 14:47:24 -0500

    commit

--- PFIT/365/Conditional Access/Powershell/Current Deploy365AIO.ps1
+++ PFIT/365/Conditional Access/Powershell/Current Deploy365AIO.ps1
@@ -977,6 +977,63 @@ if ($null -eq $Checkpolicy) {
             Write-Host "Skipped policy $PolicyName because it already exists."
         }
 
+################### Block Unsupported Platforms ##############################
+$PolicyName = "CA010: Block access for unknown or unsupported device platform"
+$Checkpolicy = Get-MgIdentityConditionalAccessPolicy -Filter "DisplayName eq '$PolicyName'"
+$ExcludeCAGroups = Get-MgGroup -top 999 -Filter "startswith(DisplayName,'SG365_Exclude_CA010: Block access for unknown or unsupported device platform')" | Select-Object ID
+if ($null -eq $Checkpolicy) {
+  $params = @{
+    DisplayName = $PolicyName
+    State = "disabled"
+    Conditions = @{
+      ClientAppTypes = @(
+        "All"
+      )
+      Applications = @{
+        IncludeApplications = @(
+          "All"
+        )
+      }
+      Platforms =@{
+        IncludePlatforms =@(
+          "All"
+        )
+        ExcludePlatforms =@(
+          "android",
+          "iOS",
+          "windows",
+          "macOS"
+        )
+      }
+      users = @{
+        IncludeUsers = @(
+          "All"
+         )
+         ExcludeUsers =@(
+          $Z1
+         )
+         ExcludeGroups = @(
+          $ExcludeCAGroups.Id
+         )
+      }
+      Locations = @{
+        IncludeLocations = @(
+          "All"
+        )
+      }
+     }
+     GrantControls = @{
+       Operator = "OR"
+       BuiltInControls = @(
+        "Block"
+       )
+     }
+  }
+  New-MgIdentityConditionalAccessPolicy -BodyParameter $params | Out-Null
+            Write-Host "Created policy $PolicyName."
+        } else {
+            Write-Host "Skipped policy $PolicyName because it already exists."
+        }
 
 ################### Require App Protection Policy IOS/Andriod ################
 $PolicyName = "CA011: Require Intune Mobile Device App Protection Policy"
@@ -1036,63 +1093,6 @@ if ($null -eq $Checkpolicy) {
             Write-Host "Skipped policy $PolicyName because it already exists."
         }
 
-################### Block Unsupported Platforms ##############################
-$PolicyName = "CA010: Block access for unknown or unsupported device platform"
-$Checkpolicy = Get-MgIdentityConditionalAccessPolicy -Filter "DisplayName eq '$PolicyName'"
-$ExcludeCAGroups = Get-MgGroup -top 999 -Filter "startswith(DisplayName,'SG365_Exclude_CA010: Block access for unknown or unsupported device platform')" | Select-Object ID
-if ($null -eq $Checkpolicy) {
-  $params = @{
-    DisplayName = $PolicyName
-    State = "disabled"
-    Conditions = @{
-      ClientAppTypes = @(
-        "All"
-      )
-      Applications = @{
-        IncludeApplications = @(
-          "All"
-        )
-      }
-      Platforms =@{
-        IncludePlatforms =@(
-          "All"
-        )
-        ExcludePlatforms =@(
-          "android",
-          "iOS",
-          "windows",
-          "macOS"
-        )
-      }
-      users = @{
-        IncludeUsers = @(
-          "All"
-         )
-         ExcludeUsers =@(
-          $Z1
-         )
-         ExcludeGroups = @(
-          $ExcludeCAGroups.Id
-         )
-      }
-      Locations = @{
-        IncludeLocations = @(
-          "All"
-        )
-      }
-     }
-     GrantControls = @{
-       Operator = "OR"
-       BuiltInControls = @(
-        "Block"
-       )
-     }
-  }
-  New-MgIdentityConditionalAccessPolicy -BodyParameter $params | Out-Null
-            Write-Host "Created policy $PolicyName."
-        } else {
-            Write-Host "Skipped policy $PolicyName because it already exists."
-        }
 
 ################### Block MFA Enrollment from Non Trusted Locations #######################
 $PolicyName = "CA012: Block MFA Enrollment from Non Trusted Locations"
